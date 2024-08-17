e6fa47cfe3a2591384700555be11cbac060d707f
jares <953533263@qq.com>
2024-08-17 15:14:00 +0800

    修改

--- 1.js
+++ 1.js
@@ -1,4 +1,15 @@
+/*
+ * @Author: jares
+ * @Date: 2024-08-17 15:12:53
+ * @LastEditors: jares
+ * @LastEditTime: 2024-08-17 15:13:31
+ * @Description: 
+ * 
+ * Copyright (c) 2024 by jares, All Rights Reserved. 
+ */
 let arr = [
     {name:'jares'}
 ]
+console.log(arr[0])
+console.log(arr[0])
 console.log(arr[0])
